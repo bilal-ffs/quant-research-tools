1a46b8ba60e2d0e455d4efb405bfab3182682fa8
bilal-ffs <beqmd135@gamil.com>
2026-08-07 22:11:22 +0530

    feat(statistics): validate returns

--- quanttools/statistics/calmar.py
+++ quanttools/statistics/calmar.py
@@ -1,1 +1,48 @@
+"""
+quanttools.statistics.calmar
+============================
 
+Functions for calculating the Calmar Ratio.
+
+References
+----------
+- Terry W. Young (1991)
+"""
+
+from __future__ import annotations
+
+import pandas as pd
+
+from quanttools.statistics.cagr import cagr
+from quanttools.statistics.drawdown import max_drawdown
+
+from quanttools.utils.validation import (
+    validate_returns,
+)
+
+
+def calmar_ratio(
+    returns: pd.Series,
+    periods_per_year: int = 252,
+) -> float:
+    """
+    Calculate the annualized Calmar Ratio.
+
+    Parameters
+    ----------
+    returns : pandas.Series
+        Periodic returns.
+
+    periods_per_year : int, default=252
+        Number of return observations per year.
+
+    Returns
+    -------
+    float
+        Annualized Calmar Ratio.
+    """
+    # Step 1: Validate input
+
+    returns = validate_returns(
+        returns
+    )
