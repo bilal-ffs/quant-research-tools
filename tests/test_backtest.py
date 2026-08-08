dc04c86b24db6972577cecf76c6d7d8f41a45b2b
bilal-ffs <beqmd135@gamil.com>
2026-08-08 19:51:20 +0530

    feat(backtest): implemented backtest API

--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
-from quanttools.backtest.backtest import (
+from quanttools import (
     Backtest,
 )
 
 
-def test_sharpe_method_returns_float():
+def create_backtest() -> Backtest:
     returns = pd.Series(
         [
-            0.01,
-            -0.02,
+            0.02,
+            -0.01,
             0.03,
+            -0.02,
             0.01,
+            0.015,
         ]
     )
 
@@ -21,14 +23,129 @@ def test_sharpe_method_returns_float():
             -50,
             75,
             -25,
+            50,
+            -40,
         ]
     )
 
-    bt = Backtest(
+    return Backtest(
         returns,
         trade_results,
     )
 
-    result = bt.sharpe_ratio()
 
-    assert isinstance(result, float)
+def test_report_returns_string():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.report(),
+        str,
+    )
+
+
+def test_sharpe_ratio_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.sharpe_ratio(),
+        float,
+    )
+
+
+def test_sortino_ratio_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.sortino_ratio(),
+        float,
+    )
+
+
+def test_cagr_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.cagr(),
+        float,
+    )
+
+
+def test_calmar_ratio_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.calmar_ratio(),
+        float,
+    )
+
+
+def test_max_drawdown_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.max_drawdown(),
+        float,
+    )
+
+
+def test_drawdown_duration_returns_int():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.drawdown_duration(),
+        int,
+    )
+
+
+def test_profit_factor_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.profit_factor(),
+        float,
+    )
+
+
+def test_expectancy_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.expectancy(),
+        float,
+    )
+
+
+def test_win_rate_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.win_rate(),
+        float,
+    )
+
+
+def test_average_win_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.average_win(),
+        float,
+    )
+
+
+def test_average_loss_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.average_loss(),
+        float,
+    )
+
+
+def test_payoff_ratio_returns_float():
+    bt = create_backtest()
+
+    assert isinstance(
+        bt.payoff_ratio(),
+        float,
+    )
