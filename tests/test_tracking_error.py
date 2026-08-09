aac03dd930eb68410918436c93fe495659548e74
bilal-ffs <beqmd135@gamil.com>
2026-08-09 18:08:42 +0530

    feat(portfolio): implement test trakcing error

--- tests/test_tracking_error.py
+++ tests/test_tracking_error.py
@@ -38,38 +38,6 @@ def test_tracking_error_returns_float():
     )
 
 
-def test_tracking_error_returns_float():
-    portfolio = pd.Series(
-        [
-            0.02,
-            -0.01,
-            0.03,
-            0.01,
-            -0.02,
-        ]
-    )
-
-    benchmark = pd.Series(
-        [
-            0.01,
-            -0.02,
-            0.02,
-            0.00,
-            -0.01,
-        ]
-    )
-
-    result = tracking_error(
-        portfolio,
-        benchmark,
-    )
-
-    assert isinstance(
-        result,
-        float,
-    )
-
-
 def test_empty_benchmark():
     portfolio = pd.Series(
         [
